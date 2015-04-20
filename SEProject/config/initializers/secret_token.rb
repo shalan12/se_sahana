ad4f4d5c17dff8d21cfc4480119222b9716c439a
saasbook <saasbook@saasbook.(none)>
2015-04-20 04:54:47 -0300

    Cucumber and Rspec added

---
+++ SEProject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SEProject::Application.config.secret_token = 'dbee324f926855fa1414a8a99adbce6d4368b9aa2e3984db40242ba9208019038c859c8bc4478fe1a2b6bf7e60be68d523efffcd576e9a649e8294977747e9c4'
