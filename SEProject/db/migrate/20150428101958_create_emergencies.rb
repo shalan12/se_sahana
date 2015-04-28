cb839d25ca0370a9104302a02a2f8699c97e7d43
Shalan <syed_shalan_naqvi@hotmail.com>
2015-04-28 07:59:07 -0300

    models added..migration done

---
+++ SEProject/db/migrate/20150428101958_create_emergencies.rb
@@ -0,0 +1,16 @@
+class CreateEmergencies < ActiveRecord::Migration
+  def up
+  	create_table :emergencies do |e|
+  		e.float 'amount_needed' 
+  		e.string 'type'
+  		e.float 'lat'
+  		e.float 'long'
+  		e.datetime 'start_time'
+  		e.datetime 'end_time'
+  	end
+  end
+
+  def down
+  	drop_table 'emergencies'
+  end
+end
