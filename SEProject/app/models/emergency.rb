e2cf2f0c71a6406291352a5c30bf50b0253e1656
Shalan <syed_shalan_naqvi@hotmail.com>
2015-04-29 06:10:29 -0300

    newpage errors fixed

--- SEProject/app/models/emergency.rb
+++ SEProject/app/models/emergency.rb
@@ -1,5 +1,5 @@
 class Emergency < ActiveRecord::Base
-  attr_accessible :amount_needed, :type, :location, :start_time, :end_time
+  attr_accessible :amount_needed, :type, :lat, :long, :start_time, :end_time
   has_many :donations
   has_many :users, through: :donations
 end
