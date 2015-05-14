3a5dbea02711de0824834c667044b88bcef8803e
Habiba-Farrukh <h.farrukh13@gmail.com>
2015-05-14 12:23:33 -0300

    Removing feature.rb files

--- SEProject/features/inference_feautre.rb
+++
@@ -1,9 +0,0 @@
-Scenario: View users who made a contribution to a particular emergency
-And I am on the homepage
-When I click on emergency "XYZ"
-Then I should see "User ABC, User BCD"
-
-Scenario: View emergencies to which a user contributed
-And I am on the usersPage
-When I click on "User XYZ"
-Then I should see "emergency ABC, emergency BCD"
