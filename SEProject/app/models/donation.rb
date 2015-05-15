8cba3d0a86f72894d67bf2313153bf185c4be5ef
Asher Anjum <asher_as@live.com>
2015-05-15 10:12:23 +0500

    moved all files to root

--- SEProject/app/models/donation.rb
+++
@@ -1,5 +0,0 @@
-class Donation < ActiveRecord::Base
-  attr_accessible :username, :emId, :amount_donated
-  belongs_to :user
-  belongs_to :emergency
-end
--- SEProject/app/models/emergency.rb
+++
@@ -1,5 +0,0 @@
-class Emergency < ActiveRecord::Base
-  attr_accessible :amount_needed, :emergency_type, :lat, :long, :start_time, :end_time
-  has_many :donations
-  has_many :users, through: :donations
-end
